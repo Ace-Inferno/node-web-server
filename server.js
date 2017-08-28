63579b7e76604465cc7a9d31fd4127970cae8383
Ace-Inferno <mathewhace@gmail.com>
2017-08-28 10:55:37 -0700

    fixed maintenance

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req,res,next) => {
-  res.render('maintenance.hbs');
-});
+//app.use((req,res,next) => {
+//  res.render('maintenance.hbs');
+//});
 
 app.use(express.static(__dirname + '/public'));
 //public needs to be here to make other pages private while under maintenance
